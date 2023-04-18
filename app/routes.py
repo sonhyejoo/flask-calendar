1323682b3e3c74cafa42e317a8ebe72f2398f676
Son Hye Joo <seongwoo.cho@gmail.com>
2023-04-17 20:32:37 -0400

    Format with black

--- app/routes.py
+++ app/routes.py
@@ -4,43 +4,51 @@ import psycopg2
 from app.forms import AppointmentForm
 from datetime import datetime
 
-bp = Blueprint('main', __name__, url_prefix='/')
+bp = Blueprint("main", __name__, url_prefix="/")
 
 
-CONNECTION_PARAMETERS ={
+CONNECTION_PARAMETERS = {
     "user": os.environ.get("DB_USER"),
     "password": os.environ.get("DB_PASS"),
     "dbname": os.environ.get("DB_NAME"),
     "host": os.environ.get("DB_HOST"),
 }
 
-@bp.route('/', methods =['GET', 'POST'])
+
+@bp.route("/", methods=["GET", "POST"])
 def main():
     form = AppointmentForm()
     if form.validate_on_submit():
         params = {
-        'name': form.name.data,
-        'start_datetime': datetime.combine(form.start_date.data, form.start_time.data),
-        'end_datetime': datetime.combine(form.end_date.data, form.end_time.data),
-        'description': form.description.data,
-        'private': form.private.data
+            "name": form.name.data,
+            "start_datetime": datetime.combine(
+                form.start_date.data, form.start_time.data
+            ),
+            "end_datetime": datetime.combine(form.end_date.data, form.end_time.data),
+            "description": form.description.data,
+            "private": form.private.data,
         }
         with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
             with conn.cursor() as curs:
-                curs.execute("""
+                curs.execute(
+                    """
                     INSERT into appointments(name, start_datetime,
                     end_datetime, description, private)
                     VALUES(%(name)s, %(start_datetime)s, %(end_datetime)s, %(description)s,
                     %(private)s
                     )
-                    """, params)
+                    """,
+                    params,
+                )
         return redirect("/")
     with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
         with conn.cursor() as curs:
-            curs.execute("""
+            curs.execute(
+                """
                 SELECT id, name, start_datetime, end_datetime
                 FROM appointments
                 ORDER BY start_datetime
-                """)
+                """
+            )
             rows = curs.fetchall()
-            return render_template('main.html', rows=rows, form=form)
+            return render_template("main.html", rows=rows, form=form)
